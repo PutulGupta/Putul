fd866162ebbdbaba4f13fc2a2d00be1c929a6c07
PutulGupta <90447393+PutulGupta@users.noreply.github.com>
2021-10-21 13:58:03 +0530

    Nested Classes

---
+++ nestedclass/Bookmark.java
@@ -0,0 +1,40 @@
+package com.jid.nestedclass;
+
+public class Bookmark {
+	private int id;
+	private String title;
+	private double rating;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+		
+
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	
+	
+
+	
+	
+	
+
+}
